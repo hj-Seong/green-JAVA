ef4fb14743cd4ee1e0b55a575cd8f64778ebdf34
hye jeong <hjseong1222@gmail.com>
2022-08-10 09:44:43 +0900

    0809 시험풀이

---
+++ 0802_test/src/test0810/Student.java
@@ -0,0 +1,39 @@
+package test0810;
+
+public class Student {
+	//필드
+	String name;
+	int number;
+	int score;
+	
+	// 생성자
+	Student() {
+		this.name = "홍길동";
+		this.number = 0;
+		this.score = 0;
+	}
+	
+	Student(String name, int number) {
+		this.name = name;
+		this.number = number;
+		this.score = 0;
+	}
+	
+	void setScore(int score) {
+		this.score = score;
+	}
+	
+	int getScore() {
+		return this.score;
+	}
+	
+	void printResult () {
+		// 60이상 합격, 미만 불합격
+		if( score >=60) {
+			System.out.println(name + "님은 합격입니다");
+		} else {
+			System.out.println(name + "님은 불합격입니다");
+		}
+	}
+
+}
---
+++ 0802_test/src/test0810/Test0810.java
@@ -0,0 +1,37 @@
+package test0810;
+
+import javax.security.auth.Subject;
+
+public class Test0810 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 50, 60, 36, 90, 100
+		// 60점 이상일 때 합격
+		// 번호는 작성된 점수 순서
+		
+		int[] score = {50, 60, 36, 90, 100};
+		// System.out.println(score[0]);
+		
+		int sum = 0;
+		int count = 0;
+		for (int i = 0 ; i<score.length ; i++ ) {
+			if(score[i] >= 60 ) {
+				System.out.println((i+1)+"번 : 합격");
+				sum += score[i];
+				count++;
+			}
+		}
+ 		System.out.println("합격자 평균 : "+(sum/count));
+		
+ 		
+ 		Student s1 = new Student();
+ 		Student s2 = new Student("흥부",1);
+ 		
+ 		s1.setScore(80);
+ 		s1.printResult();
+ 		s2.printResult();
+ 		
+	}
+
+}
